c734f3c78c55c5f5478522c5f368e816e88d1779
Hiếu Phạm <phamtrunghieu6d@gmail.com>
2021-11-18 12:30:03 +0700

    add nested resource /api/experts/field

---
+++ src/main/java/org/vietsearch/essme/repository/ExpertCustomRepositoryImpl.java
@@ -0,0 +1,59 @@
+package org.vietsearch.essme.repository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.aggregation.Aggregation;
+import org.springframework.data.mongodb.core.aggregation.Fields;
+import org.springframework.data.mongodb.core.aggregation.UnwindOperation;
+import org.springframework.stereotype.Repository;
+import org.vietsearch.essme.model.Expert;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.List;
+@Repository
+public class ExpertCustomRepositoryImpl implements ExpertCustomRepository {
+    @Autowired
+    private MongoTemplate mongoTemplate;
+
+    public Map<String, Integer> getNumberOfExpertsInEachField(){
+       List<Expert> list =  mongoTemplate.aggregate(
+                Aggregation.newAggregation(
+                        new UnwindOperation(
+                                Fields.field("_source.google_scholar_fields.fields")
+                        )
+                )
+                ,
+                Expert.class,
+                Expert.class
+        ).getMappedResults();
+
+       Map<String, Integer> map = new HashMap<>();
+
+       for(Expert expert: list){
+           changeMapValue(map, expert.getSource().getGoogleScholarFields().getFields().get(0));
+       }
+       return map;
+    }
+
+    private void changeMapValue(Map map, String key){
+        key = toUpperCaseFirstLetters(key);
+        if(!map.containsKey(key)){
+            map.put(key, Integer.valueOf(1));
+        }else{
+            Integer oldValue = (Integer) map.get(key);
+            map.replace(key, oldValue + 1);
+        }
+    }
+
+    private String toUpperCaseFirstLetters(String s){
+        String[] arr = s.split(" ");
+        StringBuffer sb = new StringBuffer();
+
+        for (int i = 0; i < arr.length; i++) {
+            sb.append(Character.toUpperCase(arr[i].charAt(0)))
+                    .append(arr[i].substring(1)).append(" ");
+        }
+        return sb.toString().trim();
+    }
+}
--- src/test/java/org/vietsearch/essme/EssmeApplicationTests.java
+++ src/test/java/org/vietsearch/essme/EssmeApplicationTests.java
@@ -3,11 +3,22 @@ package org.vietsearch.essme;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.List;
+import java.util.Arrays;
+
 @SpringBootTest
 class EssmeApplicationTests {
 
     @Test
     void contextLoads() {
+        String[] arr = "pham trung hieu".split(" ");
+        StringBuffer sb = new StringBuffer();
+
+        for (int i = 0; i < arr.length; i++) {
+            sb.append(Character.toUpperCase(arr[i].charAt(0)))
+                    .append(arr[i].substring(1)).append(" ");
+        }
+        System.out.println( sb.toString().trim());
     }
 
 }
