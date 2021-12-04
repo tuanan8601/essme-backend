e5d0b7d08818bf47da06e683113ebe25d4fdcd67
admin <tuanan8601@gmail.com>
2021-12-04 10:48:51 +0700

    Answer_question API

---
+++ src/main/java/org/vietsearch/essme/model/answer_question/Question.java
@@ -0,0 +1,52 @@
+package org.vietsearch.essme.model.answer_question;
+
+import java.util.List;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.google.api.client.util.DateTime;
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.Field;
+import org.springframework.format.annotation.DateTimeFormat;
+
+@Data
+@Document("answer_question")
+public class Question {
+
+	@JsonProperty("createdAt")
+	@Field("createdAt")
+	@DateTimeFormat
+	private DateTime createdAt;
+
+	@JsonProperty("Description")
+	@Field("Description")
+	private String description;
+
+	@JsonProperty("Customer_id")
+	@Field("Customer_id")
+	private String customerId;
+
+	@JsonProperty("answers")
+	private List<Answer> answers;
+
+	@JsonProperty("Title")
+	@Field("Title")
+	private String title;
+
+	@Id
+	@JsonProperty(value = "_id", access = JsonProperty.Access.READ_ONLY)
+	private String _id;
+
+	@JsonProperty("Topic")
+	@Field("Topic")
+	private List<String> topic;
+
+	@JsonProperty("vote")
+	@Field("vote")
+	private int vote;
+
+	@JsonProperty("updatedAt")
+	@Field("updatedAt")
+	@DateTimeFormat
+	private DateTime updatedAt;
+}
