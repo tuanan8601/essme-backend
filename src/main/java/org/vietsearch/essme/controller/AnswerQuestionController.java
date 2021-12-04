3bc785d43b211774cbe3307dbb36fb832f3c7be9
admin <tuanan8601@gmail.com>
2021-12-04 21:07:20 +0700

    Answer_question API

--- src/main/java/org/vietsearch/essme/controller/AnswerQuestionController.java
+++ src/main/java/org/vietsearch/essme/controller/AnswerQuestionController.java
@@ -31,13 +31,13 @@ public class AnswerQuestionController {
     public Question getQuestionbyId(@PathVariable("id") String id) {return questionRepository.findById(id).get();}
 
     @GetMapping
-    public List<Question> getQuestions(@RequestParam(value = "page", defaultValue = "0") int page, @RequestParam(value = "size", defaultValue = "20") int size, @RequestParam(value = "sort", defaultValue = "createdAt") String sortAttr, @RequestParam(value = "desc", defaultValue = "false") boolean desc) {
+    public Page<Question> getQuestions(@RequestParam(value = "page", defaultValue = "0") int page, @RequestParam(value = "size", defaultValue = "20") int size, @RequestParam(value = "sort", defaultValue = "createdAt") String sortAttr, @RequestParam(value = "desc", defaultValue = "false") boolean desc) {
         Sort sort = Sort.by(sortAttr);
         if (desc)
             sort = sort.descending();
 
         Page<Question> questionPage = questionRepository.findAll(PageRequest.of(page, size, sort));
-        return questionPage.getContent();
+        return questionPage;
     }
 
     @GetMapping("/search")
@@ -46,26 +46,14 @@ public class AnswerQuestionController {
         return questionRepository.findBy(criteria);
     }
 
-    @GetMapping("/totalpages")
-    public int getQuestionTotalPage(@RequestParam(value = "size", defaultValue = "20") int size) {
-        Page<Question> questionPage = questionRepository.findAll(PageRequest.of(0,size));
-        return questionPage.getTotalPages();
-    }
-
     @GetMapping("/topic/{topic}")
-    public List<Question> getQuestionsbyTopic(@PathVariable("topic") String topic,@RequestParam(value = "page", defaultValue = "0") int page, @RequestParam(value = "size", defaultValue = "20") int size, @RequestParam(value = "sort", defaultValue = "createdAt") String sortAttr, @RequestParam(value = "desc", defaultValue = "false") boolean desc) {
+    public Page<Question> getQuestionsbyTopic(@PathVariable("topic") String topic,@RequestParam(value = "page", defaultValue = "0") int page, @RequestParam(value = "size", defaultValue = "20") int size, @RequestParam(value = "sort", defaultValue = "createdAt") String sortAttr, @RequestParam(value = "desc", defaultValue = "false") boolean desc) {
         Sort sort = Sort.by(sortAttr);
         if (desc)
             sort = sort.descending();
 
         Page<Question> questionPage = questionRepository.findByTopic(topic,PageRequest.of(page, size, sort));
-        return questionPage.getContent();
-    }
-
-    @GetMapping("/topic/{topic}/totalpages")
-    public int getQuestionbyTopicTotalPage(@PathVariable("topic") String topic, @RequestParam(value = "size", defaultValue = "20") int size) {
-        Page<Question> questionPage = questionRepository.findByTopic(topic,PageRequest.of(0, size));
-        return questionPage.getTotalPages();
+        return questionPage;
     }
 
     @PostMapping
